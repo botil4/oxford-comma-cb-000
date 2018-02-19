a3e1bf40876140f57dc6082266a91ced44d55be5
Travis Clarkson <tk1667@gmail.com>
2018-02-19 02:34:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-  array.join
+  case array.length
+  when 1
+    array
+  when 2
+    array.join("and")
+  else
+    array.pop
+    array.join(",") + "and" + array.last
+  end
+
 end
